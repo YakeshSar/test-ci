91b667aaf3881e1d1e126b49a88d067b86f558fe
YakeshSar <93547089+YakeshSar@users.noreply.github.com>
2026-06-02 18:04:40 +0530

    Add files via upload
    adding testng

--- src/test/java/org/scalesoft/AppTest.java
+++ src/test/java/org/scalesoft/AppTest.java
@@ -3,11 +3,14 @@ package org.scalesoft;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class AppTest
 {
 
-    public static void main(String[] args) {
+
+    @Test
+    public  void launch() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
