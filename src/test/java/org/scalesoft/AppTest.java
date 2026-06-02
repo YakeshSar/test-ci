b4a56e57f7d491c8a0c910169b7f501d6d144d11
YakeshSar <93547089+YakeshSar@users.noreply.github.com>
2026-06-02 18:21:58 +0530

    Refactor AppTest to include ChromeOptions import

--- src/test/java/org/scalesoft/AppTest.java
+++ src/test/java/org/scalesoft/AppTest.java
@@ -3,6 +3,7 @@ package org.scalesoft;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class AppTest
